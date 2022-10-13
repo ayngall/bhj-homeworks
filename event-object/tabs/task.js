7c4d1c03c0f2bdbc68d3724b44fc7dd63e9d19ef
Aynur Gallyamov <ainyr60@gmail.com>
2022-10-13 22:10:48 +0500

    first commit variables

---
+++ event-object/tabs/task.js
@@ -0,0 +1,11 @@
+const tabs = Array.from(document.querySelectorAll(".tab"));
+const tabsContent = Array.from(document.querySelectorAll(".tab__content"));
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", function() {
+        tab.closest(".tab__navigation").querySelector(".tab_active").classList.remove("tab_active");
+        tab.classList.add("tab_active");
+        tab.closest(".tabs").querySelector(".tab__contents .tab__content_active").classList.remove("tab__content_active");
+        tabsContent[tabs.indexOf(tab)].classList.add("tab__content_active");
+    })
+})
